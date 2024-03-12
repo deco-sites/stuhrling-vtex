6457c02aa25bd945c6600284c8a251584b49f4c5
KeplerFall <kepler@myeshop.com.br>
2024-03-12 10:25:23 -0300

    header fix

--- islands/MainHeader.tsx
+++ islands/MainHeader.tsx
@@ -14,22 +14,21 @@ import type { ComponentChildren } from "preact";
 import Button from "$store/components/ui/Button.tsx";
 import { Suspense } from "preact/compat";
 
-interface SimpleDrop{
+interface SimpleDrop {
   text?: string;
   href?: string;
 }
 
-interface ComplexDropImages{
+interface ComplexDropImages {
   image?: ImageWidget;
   text?: string;
   href?: string;
 }
 
-interface ComplexDrop{
+interface ComplexDrop {
   firstRow?: ComplexDropImages[];
   secondRow?: ComplexDropImages[];
 }
-
 
 interface Props {
   searchbar?: SearchbarProps;
@@ -39,7 +38,7 @@ interface Props {
     title: string;
     link: string;
     //menuDropType?: "simpleDrop" | "complexDrop"
-    complexDrop?: ComplexDrop,
+    complexDrop?: ComplexDrop;
     //simpleDrop?: SimpleDrop
   }[];
 }
@@ -50,11 +49,14 @@ export default function MainHeader(props: Props) {
   const { displayCart } = useUI();
   const [mobileDropDown, setMobileDropDown] = useState<number[]>([]);
 
-  const updateMobileDrop = (arg)=>{
-    mobileDropDown.includes(arg) ? setMobileDropDown(mobileDropDown.filter(item => item !== arg)) : setMobileDropDown([...mobileDropDown, arg])
-  }
+  const updateMobileDrop = (arg) => {
+    mobileDropDown.includes(arg)
+      ? setMobileDropDown(mobileDropDown.filter((item) => item !== arg))
+      : setMobileDropDown([...mobileDropDown, arg]);
+  };
 
-  const dropArrow = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4175/7233677e-c5d7-4643-96c9-213e2aec7451"
+  const dropArrow =
+    "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4175/7233677e-c5d7-4643-96c9-213e2aec7451";
 
   const Aside = (
     { title, onClose, children }: {
@@ -101,38 +103,66 @@ export default function MainHeader(props: Props) {
             {headerItems
               ? headerItems.map((item) => {
                 return (
-                  <div className={`group cursor-pointer h-full flex justify-center items-center`}>
-                    <a className="font-bold text-[14px] font-['lato'] uppercase" href={item?.link}>
+                  <div
+                    className={`group cursor-pointer h-full flex justify-center items-center`}
+                  >
+                    <a
+                      className="font-bold text-[14px] font-['lato'] uppercase"
+                      href={item?.link}
+                    >
                       {item?.title}
                     </a>
-                    {/*item?.menuDropType == "complexDrop"*/ item?.complexDrop?.firstRow?.length || item?.complexDrop.secondRow?.lenght ?
-                      <div className={`z-999 w-full hidden h-fit group-hover:flex absolute top-[115px] left-0 bg-white border-b border-b-gray-300`}>
-                        <div className={`w-[1026px] my-8 mx-auto`}>
-                          <div className={`flex flex-row justify-between`}>{
-                            item?.complexDrop?.firstRow?.map(itemDrop =>{
-                              return(
-                                <a href={itemDrop?.href} className={`group/card`}>
-                                  <div className={`h-44 overflow-hidden`}>
-                                    <Image src={itemDrop.image} className={`h-full hover:scale-110 transition-all duration-[0.5s]`}></Image>
+                    {/*item?.menuDropType == "complexDrop"*/ item?.complexDrop
+                        ?.firstRow?.length ||
+                        item?.complexDrop?.secondRow?.lenght
+                      ? (
+                        <div
+                          className={`z-999 w-full hidden h-fit group-hover:flex absolute top-[115px] left-0 bg-white border-b border-b-gray-300`}
+                        >
+                          <div className={`w-[1026px] my-8 mx-auto`}>
+                            <div className={`flex flex-row justify-between`}>
+                              {item?.complexDrop?.firstRow?.map((itemDrop) => {
+                                return (
+                                  <a
+                                    href={itemDrop?.href}
+                                    className={`group/card`}
+                                  >
+                                    <div className={`h-44 overflow-hidden`}>
+                                      <Image
+                                        src={itemDrop.image}
+                                        className={`h-full hover:scale-110 transition-all duration-[0.5s]`}
+                                      >
+                                      </Image>
+                                    </div>
+                                    <p
+                                      className={`mt-3 text-center font-['lato'] font-bold text-transparent group-hover/card:text-[#FF6B00] uppercase`}
+                                    >
+                                      {itemDrop.text}
+                                    </p>
+                                  </a>
+                                );
+                              })}
+                            </div>
+                            <div className={`flex flex-row justify-between`}>
+                              {item?.complexDrop?.secondRow?.map((itemDrop) => {
+                                return (
+                                  <div
+                                    className={`h-24 w-80 bg-cover relative group/card`}
+                                  >
+                                    <Image src={itemDrop?.image}></Image>
+                                    <p
+                                      className={` z-999 translate-y-[-50%] translate-x-[-50%] left-1/2 text-center text-white group-hover/card:text-[#FF6B00] font-['lato'] font-bold absolute top-1/2`}
+                                    >
+                                      {itemDrop?.text}
+                                    </p>
                                   </div>
-                                  <p className={`mt-3 text-center font-['lato'] font-bold text-transparent group-hover/card:text-[#FF6B00] uppercase`}>{itemDrop.text}</p>
-                                </a>
-                              )
-                            })
-                          }</div>
-                          <div className={`flex flex-row justify-between`}>
-                            {item?.complexDrop?.secondRow?.map(itemDrop=>{
-                              return(
-                                <div className={`h-24 w-80 bg-cover relative group/card`}>
-                                  <Image src={itemDrop?.image}></Image>
-                                  <p className={` z-999 translate-y-[-50%] translate-x-[-50%] left-1/2 text-center text-white group-hover/card:text-[#FF6B00] font-['lato'] font-bold absolute top-1/2`}>{itemDrop?.text}</p>
-                                </div>
-                              )
-                            })}
+                                );
+                              })}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    : null}
+                      )
+                      : null}
                   </div>
                 );
               })
@@ -210,23 +240,55 @@ export default function MainHeader(props: Props) {
         <div className={`flex flex-col justify-between`}>
           {headerItems
             ? headerItems.map((item, index) => {
-              console.log(`Index: ${index}`, item)
+              console.log(`Index: ${index}`, item);
               return (
-                
                 <div className={``}>
-                  <div onClick={()=>{updateMobileDrop(index)}} className={`border-t border-gray-200 ${index + 1 == mobileDropDown?.lenght ? "border-b" : "" } py-4 pr-4 flex flex-row w-full justify-between items-center`}><p className={`font-bold text-[14px] font-['lato'] uppercase`}>{item.title}</p> {item?.complexDrop?.firstRow?.length ? <Image className={`h-[15px]  ${!mobileDropDown.includes(index) ? `-rotate-90` : null}`} height={15} src={dropArrow} /> : null}</div>
-                  <div className={`${mobileDropDown.includes(index) ? `h-fit max-h-fit` : ` max-h-0 overflow-hidden`}  transition-max-height duration-500`}>
-                    {item?.complexDrop?.firstRow?.length ? 
-                      item.complexDrop?.firstRow?.map((item, index)=>{
-                        return(
-                          <a href={item?.href}className={`border-t border-gray-200 py-4 pr-4 flex flex-row w-full justify-between items-center`}>
+                  <div
+                    onClick={() => {
+                      updateMobileDrop(index);
+                    }}
+                    className={`border-t border-gray-200 ${
+                      index + 1 == mobileDropDown?.lenght ? "border-b" : ""
+                    } py-4 pr-4 flex flex-row w-full justify-between items-center`}
+                  >
+                    <p
+                      className={`font-bold text-[14px] font-['lato'] uppercase`}
+                    >
+                      {item.title}
+                    </p>{" "}
+                    {item?.complexDrop?.firstRow?.length
+                      ? (
+                        <Image
+                          className={`h-[15px]  ${
+                            !mobileDropDown.includes(index)
+                              ? `-rotate-90`
+                              : null
+                          }`}
+                          height={15}
+                          src={dropArrow}
+                        />
+                      )
+                      : null}
+                  </div>
+                  <div
+                    className={`${
+                      mobileDropDown.includes(index)
+                        ? `h-fit max-h-fit`
+                        : ` max-h-0 overflow-hidden`
+                    }  transition-max-height duration-500`}
+                  >
+                    {item?.complexDrop?.firstRow?.length
+                      ? item.complexDrop?.firstRow?.map((item, index) => {
+                        return (
+                          <a
+                            href={item?.href}
+                            className={`border-t border-gray-200 py-4 pr-4 flex flex-row w-full justify-between items-center`}
+                          >
                             {item.text}
                           </a>
-                        )
+                        );
                       })
-                      
-                      
-                    : null}
+                      : null}
                   </div>
                 </div>
               );
--- sections/Content/BlogPageCards.tsx
+++ sections/Content/BlogPageCards.tsx
@@ -2,37 +2,39 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
 interface BlogCard {
-    image: ImageWidget;
-    cardTitle: string;
-    cardText: string;
-    saibaMaisLink: string;
-  }
-  interface Props {
-    cards: BlogCard[];
-  }
+  image: ImageWidget;
+  cardTitle: string;
+  cardText: string;
+  saibaMaisLink: string;
+}
+interface Props {
+  cards: BlogCard[];
+}
 
-export default function BlogPageCards({cards}:Props) {
-    return(
-        <section className={`my-36 xl:my-5 xl:m-auto max-w-7xl grid xl:grid-cols-3 gap-3`}>
-            {cards.map((item) => {
-                return (
-                    <div className={`w-[100vw] xl:w-auto p-2`}>
-                        <Image src={item.image} width={420} height={300} />
-                        <h3 className={`font-['lato'] text-base py-2 uppercase`}>
-                        {item.cardTitle}
-                        </h3>
-                        <p className={`font-['lato'] text-sm py-1 text-[#777]`}>
-                        {item.cardText}
-                        </p>
-                        <a
-                        className={`font-['lato'] text-base py-2 text-[#FF6B00] underline`}
-                        href={item.saibaMaisLink}
-                        >
-                        Saiba mais
-                        </a>
-                    </div>
-                    );
-                })}
-        </section>
-    )
+export default function BlogPageCards({ cards }: Props) {
+  return (
+    <section
+      className={`my-36 xl:my-5 xl:m-auto max-w-7xl grid xl:grid-cols-3 gap-3`}
+    >
+      {cards.map((item) => {
+        return (
+          <div className={`w-[100vw] xl:w-auto p-2`}>
+            <Image src={item.image} width={420} height={300} />
+            <h3 className={`font-['lato'] text-base py-2 uppercase`}>
+              {item.cardTitle}
+            </h3>
+            <p className={`font-['lato'] text-sm py-1 text-[#777]`}>
+              {item.cardText}
+            </p>
+            <a
+              className={`font-['lato'] text-base py-2 text-[#FF6B00] underline`}
+              href={item.saibaMaisLink}
+            >
+              Saiba mais
+            </a>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
